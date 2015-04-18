9fea403230176ebb5a416e70e8d4fd665b3b04a9
TechDan <tech.dan2689@gmail.com>
2015-04-18 13:28:51 -0400

    Zip files in any directory
    This script zips all files in any given directory and sub-directories.

---
+++ zip_directory.php
@@ -0,0 +1,26 @@
+<?php
+
+$zip = new ZipArchive();
+$zip->open('backup.zip', ZipArchive::CREATE);
+
+		### assign files to zip ###
+function listFolderFiles($dir) {
+	global $zip;
+	$ffs = scandir($dir);
+	foreach ($ffs as $ff) {
+		if ($ff != '.' && $ff != '..' && $ff != 'backup.zip') {
+			if (!is_dir($dir.'/'.$ff)) {
+				$zip->addFile($dir.'/'.$ff);
+				echo ($dir.'/'.$ff. " :<span style='color:green'>Success</span><br>");
+			} else {
+				listFolderFiles($dir.'/'.$ff);
+				// echo ($dir.'/'.$ff . "	<span style='color:red'>Not applicable</span>: this is a folder<br>"); //Leave this, it helped a lot for trouble-shooting!
+			}
+		}
+	}
+	return;
+}
+listFolderFiles('.');
+$zip = close();
+
+?>
